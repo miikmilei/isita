866ce4625d8b8ec4539f1b7bfc034263aa3f7e68
mikelgorrin <gorrinmikel@gmail.com>
2024-09-17 18:38:29 -0400

    first deploy

---
+++ flowers-js/main.js
@@ -0,0 +1,11 @@
+// Redireccionar después de 5 segundos
+setTimeout(function() {
+    window.location.href = "./star.html";
+}, 10000); // 5000 milisegundos = 5 segundos
+
+
+onload = () =>{
+    document.body.classList.remove("container");
+    
+};
+
